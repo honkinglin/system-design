784503c4efd8877de4681424b76bed96700f3b92
Kyrie Lin <kyrielin66@gmail.com>
2024-10-25 11:30:22 -0700

    feat: add comments

---
+++ docs/.vitepress/theme/MyLayout.vue
@@ -0,0 +1,25 @@
+<script setup>
+import DefaultTheme from "vitepress/theme";
+import { onMounted } from "vue";
+
+const { Layout } = DefaultTheme;
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.src = "https://utteranc.es/client.js";
+  script.setAttribute("repo", "honkinglin/system-design");
+  script.setAttribute("issue-term", "pathname");
+  script.setAttribute("theme", "github-light");
+  script.setAttribute("crossorigin", "anonymous");
+  script.setAttribute("async", "");
+  document.getElementById("comments").appendChild(script);
+});
+</script>
+
+<template>
+  <Layout>
+    <template #doc-after>
+      <div id="comments"></div>
+    </template>
+  </Layout>
+</template>
