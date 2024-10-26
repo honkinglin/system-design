9583205e4bd780613c1a2d6634101de338279566
Kyrie Lin <kyrielin66@gmail.com>
2024-10-26 13:56:26 -0700

    feat: vercel

--- docs/.vitepress/theme/Comment.vue
+++ docs/.vitepress/theme/Comment.vue
@@ -1,30 +1,31 @@
 <script setup>
-import { onMounted, onUnmounted } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
-const observer = new window.MutationObserver((mutationsList) => {
-  for (const mutation of mutationsList) {
-    if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-      const isDark = document.documentElement.classList.contains("dark");
-      const iframe = document.getElementById("comments").querySelector("iframe");
-      const originSrc = iframe.src;
-
-      if (isDark && originSrc.includes("theme=github-light")) {
-        iframe.src = originSrc.replace(/theme=github-(light|dark)/, "theme=github-dark");
-      }
-
-      if (!isDark && originSrc.includes("theme=github-dark")) {
-        iframe.src = originSrc.replace(/theme=github-(light|dark)/, "theme=github-light");
-      }
-    }
-  }
-});
-
-const config = { attributes: true, attributeFilter: ['class'] };
+const observer = ref(null);
 
 onMounted(() => {
   const isDark = document.documentElement.classList.contains("dark");
+  
+  const config = { attributes: true, attributeFilter: ['class'] };
+  observer.value = new window.MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+        const isDark = document.documentElement.classList.contains("dark");
+        const iframe = document.getElementById("comments").querySelector("iframe");
+        const originSrc = iframe.src;
 
-  observer.observe(document.documentElement, config);
+        if (isDark && originSrc.includes("theme=github-light")) {
+          iframe.src = originSrc.replace(/theme=github-(light|dark)/, "theme=github-dark");
+        }
+
+        if (!isDark && originSrc.includes("theme=github-dark")) {
+          iframe.src = originSrc.replace(/theme=github-(light|dark)/, "theme=github-light");
+        }
+      }
+    }
+  });
+
+  observer.value.observe(document.documentElement, config);
 
   const script = document.createElement("script");
   script.src = "https://utteranc.es/client.js";
@@ -37,7 +38,7 @@ onMounted(() => {
 });
 
 onUnmounted(() => {
-  observer.disconnect();
+  observer.value.disconnect();
 });
 </script>
 
